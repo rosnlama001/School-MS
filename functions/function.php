5921fe251da693a0724e5c1ae76d71b4be11670f
Deepak Rajbanshi <deepakrajbanshi68@gmail.com>
2020-10-10 19:42:32 +0900

    function folder added

---
+++ functions/function.php
@@ -0,0 +1,11 @@
+<?php
+include("../database/conn.php");
+get_safe_value($str){
+    $result=$conn -> real_escape_string($str);
+    $result=htmlentities($str)
+    return $result;
+}
+
+$str="<p>hello</p>";
+get_safe_value($str)
+?>
