7077f5e5d5b8686da0988fd52a809b95c305ab7c
Roshan Lama <rosnlama001@gmail.com>
2020-10-11 21:39:43 +0900

    database adn regester file update

--- database/project.sql
+++ database/project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2020 at 12:06 PM
+-- Generation Time: Oct 11, 2020 at 02:36 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.31
 
@@ -24,11 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `profile`
+-- Table structure for table `adpf`
 --
 
-CREATE TABLE `profile` (
-  `pId` int(11) NOT NULL,
+CREATE TABLE `adpf` (
+  `pfId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stpf`
+--
+
+CREATE TABLE `stpf` (
+  `pfId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trpf`
+--
+
+CREATE TABLE `trpf` (
+  `pfId` int(11) NOT NULL,
   `userId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,28 +62,38 @@ CREATE TABLE `profile` (
 
 CREATE TABLE `user` (
   `userId` int(11) NOT NULL,
-  `userName` varchar(20) NOT NULL,
+  `userName` varchar(50) NOT NULL,
   `eMail` varchar(50) NOT NULL,
-  `pass` varchar(255) NOT NULL
+  `pass` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `otp` varchar(255) NOT NULL,
+  `regDate` datetime NOT NULL,
+  `otpDate` datetime NOT NULL,
+  `flag` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userId`, `userName`, `eMail`, `pass`) VALUES
-(1, 'roshan', 'rosnlama002@gmail.com', '123456');
+INSERT INTO `user` (`userId`, `userName`, `eMail`, `pass`, `status`, `otp`, `regDate`, `otpDate`, `flag`) VALUES
+(6, 'rlama', '', '', 'teacher', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0),
+(7, 'rlama', 'rlama@sms.com', '123456', 'teacher', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0),
+(8, 'rosnlama', 'rosnlama@sms.com', '123456', 'teacher', '', '2020-10-11 18:10:24', '0000-00-00 00:00:00', 0),
+(9, 'deepak', 'deepak@sms.com', '$2y$10$ECv75.cIquugET6eNfYpteRVUfTSjjnJAABgBjTKisjyZVY9G.aKy', 'teacher', '', '2020-10-11 18:13:00', '0000-00-00 00:00:00', 0),
+(10, 'student', 'student@sms.com', '$2y$10$NyZza/oswENucnQUFET88.drpvhe/lHeEhcu0bSJV.Jntadg6EBHu', 'student', '', '2020-10-11 18:13:51', '0000-00-00 00:00:00', 0),
+(11, 'admin', 'admin@sms.com', '$2y$10$BfOlXPzDJV47DAza1VHgXeLfP7eCu9khDBB4Jqd4Fouc2j2ldSo9S', 'admin', '', '2020-10-11 18:14:27', '0000-00-00 00:00:00', 0),
+(12, 'tanaka', 'tanaka@sms.com', '$2y$10$vS6VwoPSoXr.1HcUaWEO4ex6tS/Uapi6TGmN9Hes5d13VVe/gWt2S', 'teacher', '', '2020-10-11 18:18:49', '0000-00-00 00:00:00', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `profile`
+-- Indexes for table `stpf`
 --
-ALTER TABLE `profile`
-  ADD PRIMARY KEY (`pId`),
-  ADD KEY `userToProfile` (`userId`);
+ALTER TABLE `stpf`
+  ADD PRIMARY KEY (`pfId`);
 
 --
 -- Indexes for table `user`
@@ -74,26 +106,16 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `profile`
+-- AUTO_INCREMENT for table `stpf`
 --
-ALTER TABLE `profile`
-  MODIFY `pId` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `stpf`
+  MODIFY `pfId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `profile`
---
-ALTER TABLE `profile`
-  ADD CONSTRAINT `userToProfile` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- login/php/register.php
+++ login/php/register.php
@@ -1,5 +1,19 @@
 <?php
 require_once("../../database/conn.php");
 require_once("../../functions/function.php");
-echo $_POST['status'];
-?>
+if (isset($_POST["reg"])) {
+    $status = $_POST["status"];
+    $userName = get_safe_value($mysqli, $_POST["userName"]);
+    $eMail = get_safe_value($mysqli, $_POST["eMail"]);
+    $pass = get_safe_value($mysqli, $_POST["pass"]);
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+    $sql = "insert into user (userName,eMail,pass,status,regDate) values('{$userName}','{$eMail}','{$pass}','{$status}',now());";
+    $result = $mysqli->query($sql);
+    if ($result) {
+        redirect("../html/registered.php");
+    } else {
+        echo ("its mada");
+    }
+} else {
+    redirect("../../index.html");
+}
