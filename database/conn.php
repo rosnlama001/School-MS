8c0ebc18a190c0ec86e0240d41cbf264f5fc5bba
Deepak Rajbanshi <deepakrajbanshi68@gmail.com>
2020-10-11 13:41:01 +0900

    database changed

--- database/conn.php
+++ database/conn.php
@@ -1,11 +1,2 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "project");
-// OTP generate
-$otp = "";
-for ($i = 0; $i < 4; $i++) {
-    $otp .= mt_rand(0, 9);
-}
-// time setup
-date_default_timezone_set("Asia/Tokyo");
-$time = date('Y/m/d h:i:s');
-// or die($mysqli)
+$mysqli = new mysqli("localhost", "root", "", "project") or die($mysqli);
