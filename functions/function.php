48f7c28f0351106ea0250b458b89bffdc0e830f4
Roshan Lama <rosnlama001@gmail.com>
2020-10-11 14:38:56 +0900

    up

--- functions/function.php
+++ functions/function.php
@@ -7,14 +7,15 @@ include("../database/conn.php");
 // $eMail=get_safe_value($_POST['email']);
 // 
 // -------------------------------------------------------
-    function get_safe_value($str){
-        global $mysqli;
-        $result=$mysqli -> real_escape_string($str);
-        $result=htmlentities($result);
-        return $result;
-        }
-
-    function redirect($url){
+function get_safe_value($str)
+{
+    global $mysqli;
+    $result = $mysqli->real_escape_string($str);
+    $result = htmlentities($result);
+    return $result;
+}
+function redirect($url)
+{
     return header("location:{$url}");
 }
 // redirect("../index.html");
@@ -28,23 +29,9 @@ function get_date()
 function get_otp($val)
 {
     $otp = "";
-    for ($i = 0; $i <  $val; $i++) {
+    for ($i = 0; $i < $val; $i++) {
         $otp .= mt_rand(0, 9);
     }
-    // redirect("../index.html");
-    function get_date(){
-        date_default_timezone_set("Asia/Tokyo");
-        $date=date("Y/m/d h:i:s");
-        echo $date;
-    }
-    // get_date();
-    function get_otp($val){
-        $otp = "";
-        for ($i = 0; $i < $val; $i++) {
-            $otp .= mt_rand(0, 9);
-        }
-        echo $otp;
-    }
+    echo $otp;
+}
     // get_otp(8);
-
-?>
