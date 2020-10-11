40174403f2e8d8c6194d3980f435cdc2f281d5e1
Deepak Rajbanshi <deepakrajbanshi68@gmail.com>
2020-10-11 14:29:34 +0900

    up

--- functions/function.php
+++ functions/function.php
@@ -7,7 +7,6 @@ include("../database/conn.php");
 // $eMail=get_safe_value($_POST['email']);
 // 
 // -------------------------------------------------------
-<<<<<<< HEAD
     function get_safe_value($str){
         global $mysqli;
         $result=$mysqli -> real_escape_string($str);
@@ -16,17 +15,6 @@ include("../database/conn.php");
         }
 
     function redirect($url){
-=======
-function get_safe_value($str)
-{
-    global $mysqli;
-    $result = $mysqli->real_escape_string($str);
-    $result = htmlentities($result);
-    return $result;
-}
-function redirect($url)
-{
->>>>>>> 4857d6c395b1cd6c724ef4553432983d75632fc1
     return header("location:{$url}");
 }
 // redirect("../index.html");
@@ -43,7 +31,6 @@ function get_otp($val)
     for ($i = 0; $i <  $val; $i++) {
         $otp .= mt_rand(0, 9);
     }
-<<<<<<< HEAD
     // redirect("../index.html");
     function get_date(){
         date_default_timezone_set("Asia/Tokyo");
@@ -61,8 +48,3 @@ function get_otp($val)
     // get_otp(8);
 
 ?>
-=======
-    echo $otp;
-}
-    // get_otp(5);
->>>>>>> 4857d6c395b1cd6c724ef4553432983d75632fc1
