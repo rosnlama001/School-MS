f0964b6c09a274598713eb6c80f63c6174b76755
Deepak Rajbanshi <deepakrajbanshi68@gmail.com>
2020-10-23 13:50:46 +0900

    password hash has uploaded

--- login/php/process.php
+++ login/php/process.php
@@ -11,24 +11,40 @@ if(!$_POST){
     $urlstatus =$obj1->get_safe_value($_POST['status']);
     $email =$obj1->get_safe_value($_POST['eMail']);
     $pass =$obj1->get_safe_value($_POST['pass']);
-    // $remem=$obj1->get_safe_value($_POST['remem']);
-    $condi_array = array("eMail"=>"{$email}","pass"=>"{$pass}");
+    
+    $condi_array = array("eMail"=>"{$email}");
     $row=$obj->get_data("user","",$condi_array);
     if(isset($row[0])){
         session_start();
         $_SESSION['Islogin']="yes";
          $_SESSION["status"]=$row[0]['status'];
          $_SESSION['email']=$row[0]['eMail'];
+         $_SESSION['pass']=$row[0]['pass'];
          $_SESSION['userName']=$row[0]['userName'];
-    
-         if(($row[0]['status']=="teacher" && $row[0]['status']==$urlstatus) || ($row[0]['status']=="admin"  && $row[0]['status']==$urlstatus)){
-             redirect("../../teacher/php/home.php");
-         }
-         else if($row[0]['status']=="student" && $row[0]['status']==$urlstatus){
-            redirect("../../student/php/home.php");    
-        }
-        else{
-            redirect("../../index.html");
+        //  check hash password 
+        if($pass=password_verify ($pass ,$row[0]['pass'])){
+                // rem
+                if(isset($_POST['remem'])){
+                    setcookie("email",$row[0]['eMail'],time()+60*60*24*365,"/");
+                    setcookie("pass",$row[0]['pass'],time()+60*60*24*365,"/");
+                }else{
+                    setcookie("email",$row[0]['eMail'],time()-60*60*24*366,"/");
+                    setcookie("pass",$row[0]['pass'],time()-60*60*24*366,"/");
+                }
+                // end rem
+                if(($row[0]['status']=="teacher" && $row[0]['status']==$urlstatus) || ($row[0]['status']=="admin"  && $row[0]['status']==$urlstatus)){
+                    redirect("../../teacher/php/home.php");
+                }
+                else if($row[0]['status']=="student" && $row[0]['status']==$urlstatus){
+                    redirect("../../student/php/home.php");    
+                }
+                else{
+                    redirect("../../index.html");
+                }
+
+        }else{
+            $error="メールアドレスまたはパスワードを間違っています。";
+            require_once("../html/login.php");
         }
     }
     if($row==0){
@@ -44,7 +60,7 @@ if(!$_POST){
       echo $userName =$obj1->get_safe_value($_POST['userName']);
      echo  $regeMail=$obj1->get_safe_value($_POST['regeMail']);
       echo $regpass=$obj1->get_safe_value($_POST['regpass']);
-      
+
    
 
 }
