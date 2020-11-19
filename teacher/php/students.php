84215165aca71682348338b88b4030c997739073
Deepak Rajbanshi <deepakrajbanshi68@gmail.com>
2020-11-19 21:43:11 +0900

    update

--- teacher/php/students.php
+++ teacher/php/students.php
@@ -1,8 +1,5 @@
 <?php include("../include/header.php"); ?>
-
-
-<!-- <?php //include("../html/student.php"); 
-        ?> -->
-<?php include("../include/footer.php"); ?>
+ <?php include("../html/student.php");?> 
+ <?php include("../include/footer.php"); ?>
 <script src="../assets/js/studntFormValidation.js"></script>
 <script src="../assets/js/loadpage.js"></script>
